373eba288c6d17857ebf4d018a4634b01c810596
Tam1l <tharun7s2629@gmail.com>
2026-03-11 22:36:23 +0530

    Update AppTest.java

--- src/main/java/com/example/src/test/java/com/example/AppTest.java
+++ src/main/java/com/example/src/test/java/com/example/AppTest.java
@@ -1,1 +1,18 @@
 package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testCalculateSalary() {
+
+        App app = new App();
+
+        double result = app.calculateSalary(10000);
+
+        assertEquals(13000, result, 0.01);
+
+    }
+}
