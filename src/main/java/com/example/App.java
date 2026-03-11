421e06d8e3c8d4d143fe467d5c96f8e854a35e1d
Tam1l <tharun7s2629@gmail.com>
2026-03-11 22:36:00 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,8 +2,14 @@ package com.example;
 
 public class App {
 
-    public int add(int a, int b) {
-        return a + b;
+    public double calculateSalary(double basicSalary) {
+
+        double hra = basicSalary * 0.20;   // House Rent Allowance
+        double da = basicSalary * 0.10;    // Dearness Allowance
+
+        double totalSalary = basicSalary + hra + da;
+
+        return totalSalary;
     }
 
 }
